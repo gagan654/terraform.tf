075b2929ff7fc62f51fc037e6a7eed4fe208c9ab
gagan654 <160984231+gagan654@users.noreply.github.com>
2024-06-28 22:12:38 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+ami_value = "ami-01b799c439fd5516a"
+instance_type = "t2.micro"
+subnet_id = "subnet-08bb2eeea5418420f"
---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "ami_value" {
+    description ="value for ami"
+  
+}
+
+variable "instance_type" {
+    description ="value for instance type"
+}
+
+variable "subnet_id" {
+    description = "value for subnet id"
+}
